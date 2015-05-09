b9e4d24ffa9bdd7bc94301f4c11fe7f517b5fa28
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-09 03:08:06 +0200

    improve tests

--- test.js
+++ test.js
@@ -3,14 +3,18 @@ var test = require('ava');
 var isRootPath = require('./');
 
 test(function (t) {
+	var _ = process.platform;
+
+	Object.defineProperty(process, 'darwin', {
+		value: 'win32',
+		writable: true
+	});
 	t.assert(isRootPath('/'));
 	t.assert(!isRootPath('/Users/'));
 	t.assert(!isRootPath(''));
 	t.assert(!isRootPath('.'));
 	t.assert(!isRootPath(' '));
 
-	var _ = process.platform;
-	process.platform === 'win32';
 	Object.defineProperty(process, 'platform', {
 		value: 'win32',
 		writable: true
@@ -21,7 +25,7 @@ test(function (t) {
 	t.assert(!isRootPath(''));
 	t.assert(!isRootPath('.'));
 	t.assert(!isRootPath(' '));
-	process.platform = _;
 
+	process.platform = _;
 	t.end();
 });
